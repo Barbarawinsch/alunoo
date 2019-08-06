8fbcafb87ad18c6fbb5d875f9281e06bdaf64949
Administrador <Administrador@10.10.4.20>
2019-08-06 16:48:44 -0300

    n aguento mais a barbara

---
+++ JavaApplication1/src/modelo/Alunoo.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+
+
+/**
+ *
+ * @author Administrador
+ */
+public class Alunoo {
+    private String sobrenome;
+    private String nome;
+    private String sexo;
+    private int codigo;
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Alunoo{" + "sexo=" + sexo + '}';
+    }
+    
+}
